513f19bda06127a25c941131d5c0457b0c7f71dc
RockIshtaar <rakeshguha15@gmail.com>
2020-02-23 15:46:29 +0530

    Updated after Pylint Test
    SCored 8.3/10 in Pylint

--- packager.py
+++ packager.py
@@ -1,42 +1,46 @@
-import json,os,subprocess,shutil
+import json
+import os
+import subprocess
+import shutil
 
 #Setting Up Colored ANSI Warnings
-TYELLOW='\033[33m'
-TGREEN='\033[32m'
-TRED='\033[31m'
+TYELLOW = '\033[33m'
+TGREEN = '\033[32m'
+TRED = '\033[31m'
 ENDC = '\033[m' # reset to the defaults
 
 with open('dependencies.json') as f:
-  data = json.load(f)
+    data = json.load(f)
 try:
-    cwd = str(os.getcwd())
-    os.system('rm -R '+cwd+'/package_elem')
+    CWD = str(os.getcwd())
+    os.system('rm -R '+CWD+'/package_elem')
     os.system('curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"')
     os.system('python3 get-pip.py --user')
     for elem in data['packages']:
-        runt='pip install --target '+cwd+'/package_elem '+elem
-        subprocess.check_call(runt,shell=True)
+        runt = 'pip install --target '+CWD+'/package_elem '+elem
+        subprocess.check_call(runt, shell=True)
     value = data.get('scripts')
     if value:
         print("\n************************************\\n")
         print("\nLambda Scripts found: Copying to package_elem")
-        dest=cwd+"/package_elem"
+        dest = CWD+"/package_elem"
         for elem in value:
-            shutil.copy2(elem,dest)
+            shutil.copy2(elem, dest)
         print("\n************************************\\n")
     else:
-        print('\n'+TYELLOW+'WARNING: No Lambda Handler Found.\nNOTE: Please add Lambda Hander files to the Zip'+ENDC)
+        print('\n'+TYELLOW+'WARNING: No Lambda Handler Found.'+ENDC)
+        print('\n'+TYELLOW+'NOTE: Please add Lambda Hander files to the Zip'+ENDC)
         print('\n'+TYELLOW+'Proceeding to Zip only Dependencies...'+ENDC)
     print("\n************************************\n")
     print("ZIPPING STARTS.....")
-    os.system('rm '+cwd+'/function.zip')
-    shutil.make_archive(cwd+'/function', 'zip', cwd+'/package_elem')
-    print("\n"+TGREEN+"Zip File Created at the location "+cwd+"/function.zip"+ENDC)
+    os.system('rm '+CWD+'/function.zip')
+    shutil.make_archive(CWD+'/function', 'zip', CWD+'/package_elem')
+    print("\n"+TGREEN+"Zip File Created at the location "+CWD+"/function.zip"+ENDC)
     print("\n************************************\n")
-except OSError as e:
-    print(TRED+'ERROR: '+str(e)+ENDC)
-except Exception as err:
-    print(TRED+'ERROR: '+str(err)+ENDC)    
+except OSError as ErR:
+    print(TRED+'ERROR: '+str(ErR)+ENDC)
+except Exception as ErroR:
+    print(TRED+'ERROR: '+str(ErroR)+ENDC)
 print("Cleaning up...")
-os.system('rm -R '+cwd+'/package_elem')
+os.system('rm -R '+CWD+'/package_elem')
 print("\n~End of Script~")
